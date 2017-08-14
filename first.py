52a345c7ab4889ef5dc7528ad0e03427218b4490
Leon <sirliuyang@hotmail.com>
2017-08-14 20:33:47 +0800

    Update the link to mc downloads

--- first.py
+++ first.py
@@ -1,18 +1,25 @@
 import scrapy
+from scrapy.selector import Selector
 
 class QuotesSpider(scrapy.Spider):
-    name = "quotes"
+    name = "downloads"
     start_urls = [
-        'https://en.wikipedia.org/wiki/Main_Page/',
+        'https://www.microsoft.com/en-us/download/search.aspx',
     ]
 
     def parse(self, response):
-        for div in response.css('div.quote'):
+        for eachdiv in response.css('h3'):
             yield {
-                'text': div.css('span.text::text').extract_first(),
-                'author': div.xpath('span/small/text()').extract_first(),
+                'links':eachdiv.xpath('')
+            }
+        '''
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').extract_first(),
+                'author': quote.xpath('span/small/text()').extract_first(),
             }
 
-        next_page = response.css('li.next a::attr("href")').extract_first()
+        next_page = response.css('li.next a::attr("href")').extract_first()8
         if next_page is not None:
             yield response.follow(next_page, self.parse)
+        '''
